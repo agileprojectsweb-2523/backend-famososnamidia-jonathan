c2c52ff8c6da3b8e7ea6aa555cb57717941ba573
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-08-03 15:45:16 -0300

    se der ruim voltar 4

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Rotas para usuários - a maioria deve ser protegida e/ou restrita a admins
 router.post('/', authenticateToken, authorizeRole('admin'), userController.createUser); // Admin cria usuários
 router.get('/', authenticateToken, authorizeRole('admin'), userController.getAllUsers); // Admin vê todos usuários
-router.get('/:id', authenticateToken, userController.getUserById); // Usuário logado pode ver seu perfil, ou admin pode ver qualquer um
+router.get('/:id', userController.getUserById);
 router.put('/:id', authenticateToken, userController.updateUser); // Usuário logado atualiza seu perfil, ou admin atualiza qualquer um
 router.delete('/:id', authenticateToken, authorizeRole('admin'), userController.deleteUser); // Admin deleta usuários
 
